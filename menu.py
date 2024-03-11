f28590bc30e425f2ee62b7b0b2f37d8867b2cce0
Thibault Foure <thibault.foure@outlook.com>
2024-03-11 15:22:10 +0100

    add bouton object

--- menu.py
+++ menu.py
@@ -1,3 +1,38 @@
 import pygame
 from constants import *
-from log import log
+from utility import printer
+
+
+class Button(pygame.sprite.Sprite):
+
+    def __init__(self, pos: tuple[int, int], text: str, clickable: bool = True):
+        super().__init__()
+
+        self.text = text
+
+        self.image = self.build()
+        self.rect = self.image.get_rect(topleft=pos)
+
+        self.selected = False
+
+    def build(self) -> pygame.Surface:
+
+        text_surface = printer.get_text_surface(self.text)
+
+        margin = 5
+
+        button = pygame.Surface((
+            text_surface.get_width() + margin,
+            text_surface.get_height() + margin))
+
+        button.fill('white')
+
+        button.blit(
+            text_surface,
+            (margin / 2, margin / 2))
+
+        return button
+
+    def update(self):
+
+        pass
