37ec21008ae95e17bb50642cae0c7e82af2ab739
Thibault Foure <thibault.foure@outlook.com>
2024-03-12 09:46:29 +0100

    create scene class

---
+++ scene.py
@@ -0,0 +1,14 @@
+class Scene:
+
+    def __init__(self, name):
+        
+        self.name = name
+
+    def handle_input(self):
+        pass
+
+    def update(self):
+        pass
+
+    def quit(self):
+        pass
